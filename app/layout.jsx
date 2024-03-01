47303213156303c17e1fa01f09f1245c3ce798cc
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-01 22:28:58 +0100

    Add images and assets and build navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@/assets/styles/globals.css"
+import Navbar from "@/components/Navbar"
+
 export const metadata = {
     title: "RealEstateRadar",
     description: "Find the perfect Real Estate property for you",
@@ -8,6 +10,7 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
+            <Navbar />
             <main>{children}</main>
         </body>
     </html>
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,4 @@
-
+"use client"
 
 const PropertyPage = () => {
   return (
