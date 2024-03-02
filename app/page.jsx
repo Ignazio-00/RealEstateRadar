6cc8c546dcce3608f5d96de71de248a194da69c7
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-02 23:25:38 +0100

    Component layout integrated

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
-import Link from 'next/link'
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import InfoBoxes from "../components/InfoBoxes";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <Footer />
+    </>
   )
 }
 
