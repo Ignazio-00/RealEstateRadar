531b09969a2332305852f5d29f913b4edfa864d6
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-10 20:31:50 +0100

    Add property listing functionality to PropertiesPage component and dynamic data

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,22 @@
+import porperties from "@/properties.json"
+import PropertyCard from "@/components/PropertyCard"
+
 
 const PropertiesPage = () => {
   return (
-    <div>PropertiesPage</div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {porperties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {porperties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+            </div>
+        )}
+      </div>
+    </section>
   )
 }
 
