d43719835f42f6c860c605f6193167df068ef3be
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-11 18:28:48 +0100

    Add Property model and update GET endpoint

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,10 +1,14 @@
 import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
-
+// GET /api/properties
 export const GET = async (request) => {
     try {
         await connectDB();
-        return new Response(JSON.stringify({message: "Hello World"}), { status: 200 })
+
+        const properties = await Property.find({});
+
+        return new Response(JSON.stringify(properties), { status: 200 })
     } catch (error) {
         console.log(error);
         return new Response("An error occurred", { status: 500 });
