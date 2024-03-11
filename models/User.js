ab79e5847e2f469710839716925d41c6572511a7
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-11 17:13:08 +0100

    Add User model definition

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import { Schema, model, models } from "mongoose"
+
+const UserSchema = new Schema ({
+    email: {
+        type: String,
+        unique: [true, "Email is already in use"],
+        required: [true, "Email is required"],
+    },
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+    },
+    image: {
+        type: String,
+    },
+    bookmarks: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Property",
+        },
+    ],
+
+}, {
+    timestamps: true,
+}
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
