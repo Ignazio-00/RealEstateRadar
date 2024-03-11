716f49e872111bd133f497a92b5954c26b57873a
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-03-11 17:13:01 +0100

    Add route file for properties API

---
+++ app/api/properties/route.js
@@ -0,0 +1,12 @@
+import connectDB from '@/config/database';
+
+
+export const GET = async (request) => {
+    try {
+        await connectDB();
+        return new Response(JSON.stringify({message: "Hello World"}), { status: 200 })
+    } catch (error) {
+        console.log(error);
+        return new Response("An error occurred", { status: 500 });
+    }
+}
