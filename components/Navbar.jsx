e6b32e6f2955d93c4bf39534d7a0b91962b00a82
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-04-04 15:59:07 +0200

    Add Navbar functionality and middleware configuration

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -189,6 +189,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -198,10 +201,17 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
