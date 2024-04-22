5a29de1f2b8bfd6cdc38ca69dc4034a5cee98f43
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-04-22 10:29:30 +0200

    Handle DELETE requests for individual properties

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,18 +1,51 @@
-import connectDB from '@/config/database';
-import Property from '@/models/Property';
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        const property = await Property.findById(params.id);
+    const property = await Property.findById(params.id);
 
-        if (!property) return new Response("Property not found", { status: 404 });
+    if (!property) return new Response("Property not found", { status: 404 });
 
-        return new Response(JSON.stringify(property), { status: 200 })
-    } catch (error) {
-        console.log(error);
-        return new Response("An error occurred", { status: 500 });
+    return new Response(JSON.stringify(property), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("An error occurred", { status: 500 });
+  }
+};
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    const propertyId = params.id;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
     }
-}
+
+    const { userId } = sessionUser;
+
+    await connectDB();
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) return new Response("Property not found", { status: 404 });
+
+    if (property.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await property.deleteOne();
+
+    return new Response("Property Deleted", { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("An error occurred", { status: 500 });
+  }
+};
