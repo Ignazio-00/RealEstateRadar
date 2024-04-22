1f0cf60d4c9e384ab76f0958c89068722fce74a9
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-04-22 10:39:52 +0200

    Refactor layout and profile components, add toast notifications

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "RealEstateRadar",
@@ -14,6 +17,8 @@ const MainLayout = ({ children }) => {
         <body>
           <Navbar />
           <main>{children}</main>
+          <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
