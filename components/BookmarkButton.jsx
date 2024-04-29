2624dbe740d34d87c01886e846c7be3c46b23815
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-04-29 11:23:02 +0200

    Update BookmarkButton component to use react-icons/fa in components/BookmarkButton.jsx

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,7 +1,9 @@
+import { FaBookmark } from "react-icons/fa";
+
 const BookmarkButton = () => {
   return (
     <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <i className="fas fa-bookmark mr-2"></i> Bookmark Property
+      <FaBookmark className="mr-2" /> Bookmark Property
     </button>
   );
 };
