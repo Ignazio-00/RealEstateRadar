0a088ca4ef8dac7123ec4c6420f648062f3acbe2
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-04-29 12:03:45 +0200

    Add bookmark functionality to route.js

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,47 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { propertyId } = await request.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!session || !session.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    const user = await User.findOne({ _id: userId });
+
+    let isBookmarked = user.bookmarks.includes(propertyId);
+
+    let message;
+
+    if (isBookmarked) {
+      user.bookmarks.pull(propertyId);
+      message = "Bookmark removed successfully";
+      isBookmarked = false;
+    } else {
+      user.bookmarks.push(propertyId);
+      message = "Bookmark added successfully";
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
