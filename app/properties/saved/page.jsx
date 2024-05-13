c957c186722b173d8e63e87719cba5043c6188a4
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-05-13 16:33:56 +0200

    refactor: Update SavedPropertiesPage component to display saved properties

--- app/properties/saved/page.jsx
+++ app/properties/saved/page.jsx
@@ -33,7 +33,24 @@ const SavedPropertiesPage = () => {
 
   console.log(properties);
 
-  return <div>SavedPropertiesPage</div>;
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="px-4 py-6">
+      <h1 className="text-2xl mb-4">Saved Properties</h1>
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No saved Properties</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default SavedPropertiesPage;
