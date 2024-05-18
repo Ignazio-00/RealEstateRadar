1f0d97614380bc136bf00f3d35c9edb5718e4239
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-05-18 21:16:08 +0200

    Add: Fetch search results from API

---
+++ app/properties/search-results/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+
+const SearchResultsPage = () => {
+  const searchParams = useSearchParams();
+
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const location = searchParams.get("location");
+  const propertyType = searchParams.get("propertyType");
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data);
+        } else {
+          setProperties([]);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSearchResults();
+  }, [location, propertyType]);
+
+  return <div>SearchResultsPage</div>;
+};
+
+export default SearchResultsPage;
