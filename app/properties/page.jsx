b7478a6e5952f6dad10ad6ab3601344d59fa2e21
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-05-20 15:28:35 +0200

    Add: PropertySearchForm component to main properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,6 @@
 import PropertyCard from "@/components/PropertyCard";
 import { fetchProperties } from "@/utils/requests";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
@@ -8,19 +9,27 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section class="bg-blue-700 py-4">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h1 className="text-2xl mb-4">Browse Properties</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property, index) => (
+                <PropertyCard property={property} key={index} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
