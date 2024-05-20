705de04173dc4ac6264a558bbcdd665240c76a59
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-05-20 15:15:22 +0200

    refactor: Improve search results page layout and functionality

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -2,6 +2,10 @@
 
 import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
@@ -34,7 +38,31 @@ const SearchResultsPage = () => {
     fetchSearchResults();
   }, [location, propertyType]);
 
-  return <div>SearchResultsPage</div>;
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <Link
+          href="/properties"
+          className="flex items-center text-blue-500 hover:underline mb-3"
+        >
+          <FaArrowAltCircleLeft className="mr-2 mb-1" />
+          Back to properties
+        </Link>
+        <h1 className="text-2xl mb-4">Search Results</h1>
+        {properties.length === 0 ? (
+          <p>No search results found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default SearchResultsPage;
