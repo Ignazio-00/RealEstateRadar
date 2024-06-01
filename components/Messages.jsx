b3218763ea695bdd1aa9f6cd3afe69878808a102
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-01 15:21:02 +0200

    refactor: Update Messages component to include lazy loading for images

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Spinner from "@/components/Spinner";
+import Message from "@/components/Message";
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
@@ -25,7 +26,26 @@ const Messages = () => {
     getMessages();
   }, []);
 
-  return <div>Messages</div>;
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+          <div className="space-y-4">
+            {messages.length === 0 ? (
+              <p>You have no Messages</p>
+            ) : (
+              messages.map((message) => (
+                <Message key={message._id} message={message} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Messages;
