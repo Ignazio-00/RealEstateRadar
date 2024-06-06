cc864516fa5afb445d1a36a32b040faa3385cab7
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-06 17:47:55 +0200

    refactor: Add UnreadMessageCount component to Navbar for displaying unread message count

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ import profileDefault from "@/assets/images/profile.png";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import UnreadMessageCount from "./UnreadMessageCount";
 
 const Navbar = () => {
   const { data: session } = useSession();
@@ -145,10 +146,7 @@ const Navbar = () => {
                     />
                   </svg>
                 </button>
-                <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                  2
-                  {/* <!-- Replace with the actual number of notifications --> */}
-                </span>
+                <UnreadMessageCount />
               </Link>
               {/* <!-- Profile dropdown button --> */}
               <div className="relative ml-3">
