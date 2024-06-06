7a170f88af4ac90a75fbf42292284068a69595c5
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-06 17:48:20 +0200

    feat: Add UnreadMessageCount component to display unread message count in Navbar

---
+++ components/UnreadMessageCount.jsx
@@ -0,0 +1,10 @@
+"use client";
+const UnreadMessageCount = () => {
+  return (
+    <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+      2{/* <!-- Replace with the actual number of notifications --> */}
+    </span>
+  );
+};
+
+export default UnreadMessageCount;
