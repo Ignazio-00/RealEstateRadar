bf159620d580cc2f5f2fb83a996fe29e6d788081
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-07 19:29:41 +0200

    refactor: Update layout to include GlobalProvider for managing global state

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
-import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
 import "react-toastify/dist/ReactToastify.css";
+import "@/assets/styles/globals.css";
 
 export const metadata = {
   title: "RealEstateRadar",
@@ -12,16 +13,18 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang="en">
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-          <ToastContainer />
-        </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
