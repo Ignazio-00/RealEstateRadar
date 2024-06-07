1a819a1bc8301679df4ae3da43fd79a4d9fbb715
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-07 19:27:35 +0200

    refactor: Add GlobalContext for managing unread message count state

---
+++ context/GlobalContext.js
@@ -0,0 +1,21 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+// create context
+const GlobalContext = createContext();
+
+// Create a provider
+export function GlobalProvider({ children }) {
+  const [unreadCount, setUnreadCount] = useState(0);
+
+  return (
+    <GlobalContext.Provider vlaue={{ unreadCount, setUnreadCount }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
+// Create a custom hook to access context
+export function useGlobalContext() {
+  return useContext(GlobalContext);
+}
