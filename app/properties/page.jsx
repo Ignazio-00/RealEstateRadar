cf97a02f39447904d7423e940a92c8a19dbb01e8
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-12 16:54:51 +0200

    refactor: Update PropertiesPage to use Properties component for browsing properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,13 +1,7 @@
-import PropertyCard from "@/components/PropertyCard";
-import { fetchProperties } from "@/utils/requests";
+import Properties from "@/components/Properties";
 import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
-  const properties = await fetchProperties();
-
-  // Sort properties by date
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   return (
     <>
       <section class="bg-blue-700 py-4">
@@ -15,20 +9,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          <h1 className="text-2xl mb-4">Browse Properties</h1>
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property, index) => (
-                <PropertyCard property={property} key={index} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
