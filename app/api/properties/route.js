4b8b29aaf1c63c374ce1e26396b9a2912d7a26d8
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-15 14:27:12 +0200

    refactor: Fix pagination bug in GET route of properties API

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -8,8 +8,8 @@ export const GET = async (request) => {
   try {
     await connectDB();
 
-    const page = request.nextUrl.searchParams.get("page" || 1);
-    const pageSize = request.nextUrl.searchParams.get("pageSize" || 3);
+    const page = request.nextUrl.searchParams.get("page") || 1;
+    const pageSize = request.nextUrl.searchParams.get("pageSize") || 3;
 
     const skip = (page - 1) * pageSize;
 
