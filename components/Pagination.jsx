e6b3e10c3ea4467430c961ef1ead8cba6922b1c2
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-17 18:16:07 +0200

    refactor: Update Pagination component to handle disabled state for previous and next buttons

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,17 +1,27 @@
 const Pagination = ({ page, pageSize, totalItems, onPageChange }) => {
   const totalPages = Math.ceil(totalItems / pageSize);
 
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage);
+    }
+  };
+
   return (
     <section className="container mx-auto flex justify-center items-center my-8">
       <button
         className="mr-2 px-2 py-1 border border-gray-300 rounded"
+        onClick={() => handlePageChange(page - 1)}
         disabled={page === 1}
       >
         Previous
       </button>
-      <span className="mx-2">Page 1 of 10</span>
+      <span className="mx-2">
+        Page {page} of {totalPages}
+      </span>
       <button
         className="ml-2 px-2 py-1 border border-gray-300 rounded"
+        onClick={() => handlePageChange(page + 1)}
         disabled={page === totalPages}
       >
         Next
