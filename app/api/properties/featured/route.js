b5fa773ceaf4f6a5a4e3fb244b9f355599d7439a
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-20 12:31:37 +0200

    refactor: Add API route for fetching featured properties

---
+++ app/api/properties/featured/route.js
@@ -0,0 +1,18 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+// GET /api/properties/featured
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const properties = await Property.find({
+      is_featured: true,
+    });
+
+    return new Response(JSON.stringify(properties), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("An error occurred", { status: 500 });
+  }
+};
