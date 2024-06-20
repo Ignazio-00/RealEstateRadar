1bad75b01c43b8e951c81778b7119f58b818ca40
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2024-06-20 12:32:57 +0200

    refactor: Add FeaturedProperties component to HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
-import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import InfoBoxes from "../components/InfoBoxes";
 import FeaturedPropertyCard from "@/components/FeaturedPropertyCard";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <FeaturedPropertyCard />
     </>
   );
