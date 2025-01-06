7326c33bf6df8fe5bebcef7a472a67d2379e95dc
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2025-01-06 16:55:55 +0100

    Bugfix

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -4,8 +4,8 @@ import PropertySearchForm from "@/components/PropertySearchForm";
 const PropertiesPage = async () => {
   return (
     <>
-      <section class="bg-blue-700 py-4">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
           <PropertySearchForm />
         </div>
       </section>
